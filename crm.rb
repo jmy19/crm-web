3ba5d2da43fe6df741854c7f97dbe67267578688
jmy19 <jasonmyers@rogers.com>
2015-01-27 13:44:04 -0500

    added list of contacts to /contact

--- crm.rb
+++ crm.rb
@@ -1,13 +1,18 @@
 require 'sinatra'
-require 'contact'
+require_relative 'contact'
 
- 
+
 get '/' do
 	@crm_app = "My CRM"
   erb :index
 end
 
 get '/contacts' do
+	@contacts = []
+  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+
 	erb :contacts
 end
 
