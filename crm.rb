a49a9a20dcb6ae33ac226ebbf3dcd9d17acedcae
jmy19 <jasonmyers@rogers.com>
2015-01-27 13:58:21 -0500

    added form to accept contacts, added link to contact page to add further contacts

--- crm.rb
+++ crm.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require_relative 'contact'
+require_relative 'rolodex'
 
+$rolodex = Rolodex.new
 
 get '/' do
 	@crm_app = "My CRM"
@@ -8,14 +10,15 @@ get '/' do
 end
 
 get '/contacts' do
-	@contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-
 	erb :contacts
 end
 
-get '/contacts/new' do
-  erb :new
+get '/contacts/new_contact' do
+  erb :new_contact
+end
+
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.add_contact(new_contact)
+  redirect to('/contacts')
 end
