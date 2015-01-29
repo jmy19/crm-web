f998abdc6dcc3acd3f5107e9222f6223118486a8
jmy19 <jasonmyers@rogers.com>
2015-01-29 14:33:55 -0500

    added edit

--- crm.rb
+++ crm.rb
@@ -28,9 +28,35 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
-get '/contacts/1000' do 
-@contact = $rolodex.find_contact(1000)
-erb :show_contact
+get '/contacts/:id' do 
+@contact = $rolodex.find_contact(params[:id].to_i)
+	if @contact
+		erb :show_contact
+	else
+		raise Sinatra::NotFound 
+	end
 end
 
 
+get "/contacts/:id/edit" do
+  @contact = $rolodex.find_contact(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put "/contacts/:id" do
+  @contact = $rolodex.find_contact(params[:id].to_i)
+  if @contact
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.note = params[:note]
+
+    redirect to("/contacts")
+  else
+    raise Sinatra::NotFound
+  end
+end
